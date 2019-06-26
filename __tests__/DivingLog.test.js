f5fa25d5193ba4b9fac0cd57465df8b24f7064eb
Carlo Panzi <carlo.panzi@gmail.com>
2019-06-26 21:58:42 +0200

    chore: DivingLog tests for schema

--- __tests__/DivingLog.test.js
+++ __tests__/DivingLog.test.js
@@ -6,11 +6,27 @@ const fs = require('fs');
 const MOCK_FILE = path.join(__dirname, '__mocks__', 'DivingLog.xml');
 const MOCK_DATA = fs.readFileSync(MOCK_FILE, 'utf8');
 
-describe('MacDive importer', () => {
+describe('DiveLog importer', () => {
   test('Basic', async () => {
     const mock = await parse(MOCK_DATA);
 
     const logbook = importer(mock);
     expect(logbook).toMatchSnapshot();
+  });
+
+  test('Schema is valid', async () => {
+    const mock = await parse(MOCK_DATA);
+    const logbook = importer(mock);
+    const [dive] = logbook.dives;
+
+    const Ajv = require('ajv');
+    const ajv = new Ajv();
+    const validate = ajv.compile(require('../dive-schema.json'));
+    const valid = validate(dive);
+
+    if (!valid) {
+      console.log('@@@', validate.errors);
+    }
+    expect(valid).toBe(true);
   });
 });
