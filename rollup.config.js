f66b22488655bff5cbab5a483a768a3e49914cce
Carlo <carlo.panzi@gmail.com>
2022-11-07 09:03:39 +0100

    build: Adapted to rollup 3

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,8 @@
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 
-const packageJson = require('./package.json');
-const external = Object.keys(packageJson.dependencies || {});
-
 export default {
   input: './src/index.ts',
-  external,
   output: [
     {
       file: 'build/index.js',
