b5a4b2f7eb6271d35a5577494199b4e62adf64b0
Carlo <carlo.panzi@gmail.com>
2022-11-07 09:25:14 +0100

    build: Removed rollup and switched to basic ts to build

--- __tests__/MacDive.test.ts
+++ __tests__/MacDive.test.ts
@@ -1,16 +1,16 @@
 import macdive from '../src/macdive';
 
-import path from 'path';
-import fs from 'fs';
+import { join } from 'node:path';
+import { readFileSync } from 'node:fs';
 
 const { importer } = macdive;
 
-const MOCK_FILE = path.join(__dirname, '__mocks__', 'macdive', 'MacDive.xml');
-const MOCK_DATA = fs.readFileSync(MOCK_FILE, 'utf8');
+const MOCK_FILE = join(__dirname, '__mocks__', 'macdive', 'MacDive.xml');
+const MOCK_DATA = readFileSync(MOCK_FILE, 'utf8');
 
 // missing
-const MOCK_FILE_MISSING_PROPS = path.join(__dirname, '__mocks__', 'macdive', 'MissingProps.xml');
-const MOCK_DATA_MISSING = fs.readFileSync(MOCK_FILE_MISSING_PROPS, 'utf8');
+const MOCK_FILE_MISSING_PROPS = join(__dirname, '__mocks__', 'macdive', 'MissingProps.xml');
+const MOCK_DATA_MISSING = readFileSync(MOCK_FILE_MISSING_PROPS, 'utf8');
 
 describe('MacDive importer', () => {
   test('Basic', async () => {
@@ -19,8 +19,8 @@ describe('MacDive importer', () => {
   });
 
   test('Failing for missing props', async () => {
-    const MOCK_FILE = path.join(__dirname, '__mocks__', 'macdive', 'MissingProps.xml');
-    const MOCK_DATA = fs.readFileSync(MOCK_FILE, 'utf8');
+    const MOCK_FILE = join(__dirname, '__mocks__', 'macdive', 'MissingProps.xml');
+    const MOCK_DATA = readFileSync(MOCK_FILE, 'utf8');
 
     expect(() => {
       macdive.importer(MOCK_DATA);
--- __tests__/validation.test.ts
+++ __tests__/validation.test.ts
@@ -1,17 +1,17 @@
 import { importer } from '../src';
-import path from 'path';
-import fs from 'fs';
+import { join } from 'node:path';
+import fs, { readdirSync, readFileSync, statSync } from 'node:fs';
 import Ajv from 'ajv';
 
-const MOCK_FOLDER = path.join(__dirname, '__mocks__');
+const MOCK_FOLDER = join(__dirname, '__mocks__');
 
 // List all files in a directory in Node.js recursively in a synchronous fashion
 const walkSync = function (dir: string, filelist: string[] = []) {
-  const files = fs.readdirSync(dir);
+  const files = readdirSync(dir);
 
   files.forEach(function (file) {
-    const filePath = path.join(dir, file);
-    if (fs.statSync(filePath).isDirectory()) {
+    const filePath = join(dir, file);
+    if (statSync(filePath).isDirectory()) {
       filelist = walkSync(filePath, filelist);
     } else if (filePath.match(/\.xml$/)) {
       filelist.push(filePath);
@@ -30,9 +30,8 @@ describe('JSON Schema Validation', () => {
 
   files.forEach((file) => {
     test(file, () => {
-      const data = fs.readFileSync(file, 'utf8');
+      const data = readFileSync(file, 'utf8');
       const logbook = importer(data);
-
 
       const valid = validate(logbook);
       if (!valid) {
