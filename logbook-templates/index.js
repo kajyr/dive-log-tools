13d524d0b3857556ea8c3446bc2e2ee05ec46495
Carlo <carlo.panzi@gmail.com>
2022-11-08 12:01:20 +0100

    chore: updated mclip

--- logbook-templates/index.js
+++ logbook-templates/index.js
@@ -7,47 +7,47 @@ const pdfkit = require('./lib/pdfkit');
 const { readFile } = require('fs/promises');
 
 const EMPTY_LOGBOOK = {
-    dives: [
-        {
-            gases: [{ pressureStart: '', pressureEnd: '' }],
-            tags: [],
-            entry: '',
-            date: null,
-            location: {},
-            types: [],
-            samples: [],
-            entry_time: null,
-        },
-    ],
+  dives: [
+    {
+      gases: [{ pressureStart: '', pressureEnd: '' }],
+      tags: [],
+      entry: '',
+      date: null,
+      location: {},
+      types: [],
+      samples: [],
+      entry_time: null,
+    },
+  ],
 };
 
 async function convert(file, dest, options) {
-    const xml = await readFile(file, 'utf8');
-    const logbook = await importer(xml);
+  const xml = await readFile(file, 'utf8');
+  const logbook = await importer(xml);
 
-    return process(logbook, dest, options);
+  return process(logbook, dest, options);
 }
 
 async function convertEmpty(dest, options) {
-    if (options.verbose) {
-        console.log('Rendering empty template');
-    }
+  if (options.verbose) {
+    console.log('Rendering empty template');
+  }
 
-    return process(EMPTY_LOGBOOK, dest, options);
+  return process(EMPTY_LOGBOOK, dest, options);
 }
 
 async function process(logbook, dest, options) {
-    const enriched = await enrich(logbook, options);
+  const enriched = await enrich(logbook, options);
 
-    pdfkit(enriched, dest, options);
+  pdfkit(enriched, dest, options);
 
-    if (options.debug) {
-        const jsonDebugFile = path.join(destFolder, `logbook.json`);
-        await saveJson(jsonDebugFile, enriched);
-    }
+  if (options.debug) {
+    const jsonDebugFile = path.join(destFolder, `logbook.json`);
+    await saveJson(jsonDebugFile, enriched);
+  }
 }
 
 module.exports = {
-    convert,
-    convertEmpty,
+  convert,
+  convertEmpty,
 };
