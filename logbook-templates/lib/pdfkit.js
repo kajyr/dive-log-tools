a193ac06eba20dcad6c821edd321e0fa7d1af0fd
Carlo <carlo.panzi@gmail.com>
2022-11-16 16:21:33 +0100

    chore: Initial integration with ts-node

--- logbook-templates/lib/pdfkit.js
+++ logbook-templates/lib/pdfkit.js
@@ -8,36 +8,36 @@ const page = require('./pages/ara-didattica');
 const PACKAGE_PATH = path.normalize(path.join(__dirname, '../package.json'));
 
 function init(logbook, dest) {
-    const package = JSON.parse(readFileSync(PACKAGE_PATH, 'utf8'));
+  const pkg = JSON.parse(readFileSync(PACKAGE_PATH, 'utf8'));
 
-    // Create a document
-    const doc = new PDFDocument({
-        autoFirstPage: false,
-        permissions: {
-            printing: 'highResolution',
-            modifying: false,
-        },
-    });
-    doc.pipe(createWriteStream(dest));
+  // Create a document
+  const doc = new PDFDocument({
+    autoFirstPage: false,
+    permissions: {
+      printing: 'highResolution',
+      modifying: false,
+    },
+  });
+  doc.pipe(createWriteStream(dest));
 
-    // Set some meta data
-    doc.info['Title'] = 'FIPSAS Logbook';
+  // Set some meta data
+  doc.info['Title'] = 'FIPSAS Logbook';
 
-    doc.info['Author'] = 'Carlo Panzi';
+  doc.info['Author'] = 'Carlo Panzi';
 
-    doc.font('Helvetica');
-    doc.fontSize(12);
+  doc.font('Helvetica');
+  doc.fontSize(12);
 
-    logbook.dives.reverse().forEach((dive) => {
-        try {
-            page(doc, dive, { version: package.version });
-        } catch (e) {
-            console.log('error', e);
-        }
-    });
+  logbook.dives.reverse().forEach((dive) => {
+    try {
+      page(doc, dive, { version: pkg.version });
+    } catch (e) {
+      console.log('error', e);
+    }
+  });
 
-    // Finalize PDF file
-    doc.end();
+  // Finalize PDF file
+  doc.end();
 }
 
 module.exports = init;
